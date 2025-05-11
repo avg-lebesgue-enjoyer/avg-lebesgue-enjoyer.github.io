88946745f24968f5d37803e0afa357f583ff825e
Gabe Field <gabriel.field@uqconnect.edu.au>
2025-05-11 23:47:50 +1000

    Partial progress on JS for Proof-family pages

--- script/discussion.js
+++ script/discussion.js
@@ -58,22 +58,71 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
+  /* SECTION: Amend `.frame-number` field of some interactive diagram */
+  /**
+   * Compute whether the given `element`'s top-left corner is on-screen.
+   * @param {Element} element
+   *  The element to check for visibility
+   * @returns {boolean}
+   *  `true` just when the top-left corner of the `element` is on-screen.
+   */
+  const isOnScreen = (element) => {
+    let rect = element.getBoundingClientRect();
+    return rect.x >= 0 && rect.y >= 0;
+  }
+
+  /**
+   * Get an array containing all of the interactive diagrams currently on-screen, sorted top-to-bottom.
+   * @returns {Array<Element>}
+   *  The array of all interactive diagrams currently on-screen.
+   */
+  const getOnScreenInteractiveDiagrams = () => {
+    return document.getElementsByClassName("interactive-diagram-container").filter(isOnScreen);
+  }
+
+  /**
+   * Append the given `key : string` to the `.frame-number` field of the topmost interactive diagram
+   * currently on screen. If there are no such diagrams on screen, then do nothing.
+   * @param {string} key
+   *  The keypress to be appended to the relevant `.frame-number` field.
+   */
+  const appendToFrameNumber = (key) => {
+    let diagrams = getOnScreenInteractiveDiagrams();
+    if (diagrams.length === 0) {
+      return;
+    }
+    const [diagram, ...ignoreThese] = diagrams;
+    
+  }
+
+
+
   /* SECTION: Event handlers */
 
   /**
    * Handle keypresses by the user. The following actions are supported:
-   *  `")"`:          Jump to top
-   *  `"!"`:          Jump to the section at `sectionIds[0]`
-   *  `"@"`:          Jump to the section at `sectionIds[1]`
-   *  `"#"`:          Jump to the section at `sectionIds[2]`
-   *  `"$"`:          Jump to the section at `sectionIds[3]`
-   *  `"%"`:          Jump to the section at `sectionIds[4]`
-   *  `"^"`:          Jump to the section at `sectionIds[5]`
-   *  `"&"`:          Jump to the section at `sectionIds[6]`
-   *  `"*"`:          Jump to the section at `sectionIds[7]`
-   *  `"("`:          Jump to the section at `sectionIds[secitonIds.length - 1]`
-   *  `"c"`, `"a"`:   Toggle contents bar
-   *  `"s"`, `"f"`:   Toggle sidenotes bar
+   *  SHIFT + NUMBER:
+   *    ")":  Jump to top
+   *    "!":  Jump to the section at `sectionIds[0]`
+   *    "@":  Jump to the section at `sectionIds[1]`
+   *    "#":  Jump to the section at `sectionIds[2]`
+   *    "$":  Jump to the section at `sectionIds[3]`
+   *    "%":  Jump to the section at `sectionIds[4]`
+   *    "^":  Jump to the section at `sectionIds[5]`
+   *    "&":  Jump to the section at `sectionIds[6]`
+   *    "*":  Jump to the section at `sectionIds[7]`
+   *    "(":  Jump to the section at `sectionIds[secitonIds.length - 1]`
+   *  NUMBER:
+   *    "0", "1", "2":  Focus the `.frame-number` input of the topmost on-screen
+   *    "3", "4", "5":   interactive diagram, and append the string `"0"`/.../`"9"`
+   *    "6", "7", "8":   to its value. Only useful on Proof-family pages.
+   *    "9"          :
+   *    " ":            Focus the `.frame-number` input of the topmost on-screen
+   *                     interactive diagram, and reset its value to `""`. Only
+   *                     useful on Proof-family pages.
+   *  LETTER:
+   *    "c", "a": Toggle contents bar
+   *    "s", "f": Toggle sidenotes bar
    * All alphabetic keys are case-insensitive.
    * @param {KeyboardEvent} e
    *  The event triggering this callback
@@ -81,7 +130,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const handleKeyDown = (e) => {
     // Act on the relevant key press
     // Enclosed in a `try { ... } catch { ... }` because there may not be enough sections to match the key pressed,
-    // if the key is one of `"qwertyuiop"`.
+    // if the key is one of `"!@#$%^&*()"`. This squashes index-out-of-bounds-errors.
     try {
       switch (e.key.toLowerCase()) {
         // Jump to section
@@ -114,6 +163,22 @@ document.addEventListener("DOMContentLoaded", () => {
           break;
         case ")":
           jumpToTop();
+          break;
+        // Type into `.frame-number` field
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+          appendToFrameNumber(e.key.toLowerCase());
+          break;
+        case " ":
+          resetFrameNumber();
           break;
         // Toggle sidebars
         case "c":
